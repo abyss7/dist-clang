982af4dd668d07b09bdb92a9a42ea51d3dd788f7
ilezhankin <ilezhankin@yandex-team.ru>
2013-10-10 21:40:26 +0400

    Demangle function names in backtrace on assertion.

--- src/base/assert.h
+++ src/base/assert.h
@@ -3,7 +3,44 @@
 #include <iostream>
 #include <sstream>
 
+#include <cxxabi.h>
 #include <execinfo.h>
+
+namespace {
+
+std::pair<std::string, uint64_t> Demangle(const char* backtrace_symbol) {
+  std::string string = backtrace_symbol;
+
+  auto begin_name = string.find('(');
+  if (begin_name == std::string::npos) {
+    return std::make_pair(string, 0);
+  }
+  begin_name++;
+
+  auto end_name = string.find('+', begin_name);
+  if (end_name == std::string::npos) {
+    return std::make_pair(string, 0);
+  }
+
+  std::string mangled_name = string.substr(begin_name, end_name - begin_name);
+  size_t size = 256;
+  int status;
+  char* demangled_name =
+      abi::__cxa_demangle(mangled_name.c_str(), nullptr, &size, &status);
+  if (status == 0) {
+    auto result = std::make_pair(std::string(demangled_name), 0);
+    free(demangled_name);
+    return result;
+  }
+  else {
+    if (demangled_name) {
+      free(demangled_name);
+    }
+    return std::make_pair(mangled_name, 0);
+  }
+}
+
+}  // namespace
 
 namespace dist_clang {
 namespace base {
@@ -14,13 +51,13 @@ inline void Assert(bool expr) {
     std::string output;
     std::stringstream ss(output);
     const int BUFFER_SIZE = 3;
-    void* buffer[BUFFER_SIZE];
+    void* buffer[BUFFER_SIZE + 1];
 
     ss << "Assertion failed:" << std::endl;
-    auto size = backtrace(buffer, BUFFER_SIZE);
+    auto size = backtrace(buffer, BUFFER_SIZE + 1);
     auto strings = backtrace_symbols(buffer, size);
-    for (int i = 0; i < size; ++i) {
-      ss << "  " << strings[i] << std::endl;
+    for (int i = 1; i < size; ++i) {
+      ss << "  " << Demangle(strings[i]).first << std::endl;
     }
     free(strings);
 
