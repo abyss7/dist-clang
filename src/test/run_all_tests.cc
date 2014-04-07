cbc421832516a747a60c45dc356f7aedda921591
Ivan Lezhankin <ilezhankin@yandex-team.ru>
2014-04-07 10:43:44 +0400

    Remove unused 'gmock' library.

--- src/test/run_all_tests.cc
+++ src/test/run_all_tests.cc
@@ -1,10 +1,10 @@
-#include <gmock/gmock.h>
+#include <gtest/gtest.h>
 
 int main(int argc, char* argv[]) {
   // Ignore SIGPIPE to prevent application crashes.
   signal(SIGPIPE, SIG_IGN);
 
-  ::testing::InitGoogleMock(&argc, argv);
+  ::testing::InitGoogleTest(&argc, argv);
   ::testing::FLAGS_gtest_death_test_style = "fast";
   return RUN_ALL_TESTS();
 }
