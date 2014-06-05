730ac9519bf1d765ed71f330de4899649afeeff3
Ivan Lezhankin <ilezhankin@yandex-team.ru>
2014-06-05 15:46:10 +0400

    Implement building RPM-package.

---
+++ build/rpm_package.py
@@ -0,0 +1,32 @@
+import os
+import shutil
+import subprocess
+import sys
+
+argv = sys.argv[1:]
+product_dir = argv[0]
+version = argv[1]
+top_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), "..")
+
+execfile(os.path.join(top_dir, "build", "common_package.include"))
+MakeInstall(top_dir, product_dir, "rpm")
+
+# Create RPM-spec file
+args = ['sh']
+args.append(os.path.join(top_dir, "build", "expand_env_vars.sh"))
+args.append(os.path.join(top_dir, "build", "rpm_spec.template"))
+args.append(os.path.join(product_dir, "rpm.spec"))
+subprocess.Popen(args, env = {'VERSION': version}).wait()
+
+# Setup rpmbuild top directory
+shutil.rmtree(os.path.join(product_dir, "rpmbuild"))
+
+# Create .rpm file
+args = ['rpmbuild', '-bb', '--buildroot']
+args.append(os.path.join(product_dir, "rpm"))
+args.append('--define')
+args.append('_topdir ' + os.path.join(product_dir, "rpmbuild"))
+args.append('--target')
+args.append('x86_64')
+args.append(os.path.join(product_dir, "rpm.spec"))
+subprocess.Popen(args).wait()
