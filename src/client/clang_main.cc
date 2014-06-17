a54809f14a18aba73878b0b00dd118df8b5bf253
Ivan Lezhankin <ilezhankin@yandex-team.ru>
2014-06-17 13:16:02 +0400

    Micro-optimizations in client part.

--- src/client/clang_main.cc
+++ src/client/clang_main.cc
@@ -13,8 +13,7 @@ using namespace dist_clang;
 
 namespace {
 
-int ExecuteLocally(char* argv[]) {
-  String clangd_cxx_path = base::GetEnv(base::kEnvClangdCxx);
+int ExecuteLocally(char* argv[], const String& clangd_cxx_path) {
   if (clangd_cxx_path.empty()) {
     LOG(FATAL) << "Provide real clang++ compiler path via "
                << base::kEnvClangdCxx;
@@ -37,18 +36,22 @@ int main(int argc, char* argv[]) {
   // It's safe to use |base::Log|, since its internal static objects don't need
   // special destruction on |exec|.
   String clangd_log_levels = base::GetEnv(base::kEnvClangdLogLevels);
-  String clangd_log_mark = base::GetEnv(base::kEnvClangdLogMark);
-  List<String> numbers;
 
-  base::SplitString<' '>(clangd_log_levels, numbers);
-  if (numbers.size() % 2 == 0) {
-    base::Log::RangeSet ranges;
-    for (auto number = numbers.begin(); number != numbers.end(); ++number) {
-      ui32 left = base::StringTo<ui32>(*number++);
-      ui32 right = base::StringTo<ui32>(*number);
-      ranges.emplace(right, left);
+  if (!clangd_log_levels.empty()) {
+    String clangd_log_mark = base::GetEnv(base::kEnvClangdLogMark);
+    List<String> numbers;
+
+    base::SplitString<' '>(clangd_log_levels, numbers);
+    if (numbers.size() % 2 == 0) {
+      base::Log::RangeSet ranges;
+      for (auto number = numbers.begin(); number != numbers.end(); ++number) {
+        ui32 left = base::StringTo<ui32>(*number++);
+        ui32 right = base::StringTo<ui32>(*number);
+        ranges.emplace(right, left);
+      }
+      base::Log::Reset(base::StringTo<ui32>(clangd_log_mark),
+                       std::move(ranges));
     }
-    base::Log::Reset(base::StringTo<ui32>(clangd_log_mark), std::move(ranges));
   }
 
   // NOTICE: Use separate |DoMain| function to make sure that all local objects
@@ -58,7 +61,7 @@ int main(int argc, char* argv[]) {
   String socket_path =
       base::GetEnv(base::kEnvClangdSocket, base::kDefaultClangdSocket);
   if (client::DoMain(argc, argv, socket_path, clang_path)) {
-    return ExecuteLocally(argv);
+    return ExecuteLocally(argv, clang_path);
   }
   return 0;
 }
