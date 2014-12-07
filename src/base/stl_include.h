9608d45e95d914d723e65cff8dad93b91317f291
Ivan Lezhankin <ilezhankin@yandex-team.ru>
2014-12-07 01:24:40 -0800

    Disable exceptions on Windows
    When -fno-exceptions is specified.

--- src/base/stl_include.h
+++ src/base/stl_include.h
@@ -3,5 +3,9 @@
 #if !defined(OS_WIN)
 #define STL(file_name) <third_party/libcxx/exported/include/file_name>
 #else
+#if !__has_feature(cxx_exceptions)
+#define _HAS_EXCEPTIONS 0
+#define __uncaught_exception std::uncaught_exception
+#endif  // !__has_feature(cxx_exceptions)
 #define STL(file_name) <file_name>
 #endif  // !defined(OS_WIN)
