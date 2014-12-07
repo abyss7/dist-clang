d2a95ccb7a0c1b363a77e57d5e1436bababa0c01
Ivan Lezhankin <ilezhankin@yandex-team.ru>
2014-12-07 08:37:36 -0800

    Update Windows configuration

---
+++ build/configure.bat
@@ -0,0 +1,5 @@
+set gn_path="%~dp0..\bin\win\gn.exe"
+
+%gn_path% --check --args="config_for_debug=true %*" gen //out/Debug.gn
+%gn_path% --check --args="%*" gen //out/Release.gn
+%gn_path% --check --args="config_for_tests=true %*" gen //out/Release.gn
