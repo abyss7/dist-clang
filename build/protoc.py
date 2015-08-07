85c2156086ae5bf5a24675bd5538437e6fd28b6d
Ivan Lezhankin <ilezhankin@yandex-team.ru>
2015-08-07 15:20:47 +0300

    Use local protobuf compiler.

--- build/protoc.py
+++ build/protoc.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import os
 import subprocess
 import sys
 
-subprocess.check_call(['protoc', '--proto_path=' + sys.argv[1],
-                       '--cpp_out=' + sys.argv[2],
-                       '--python_out=' + sys.argv[2], sys.argv[3]])
+# Hack for running 'protoc' in test configuration.
+env = os.environ
+env['LSAN_OPTIONS'] = 'detect_leaks=0'
+
+subprocess.Popen(['./protoc', '--proto_path=' + sys.argv[1],
+                  '--cpp_out=' + sys.argv[2],
+                  '--python_out=' + sys.argv[2], sys.argv[3]], env=env)
