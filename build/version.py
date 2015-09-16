311d6f1cc672cea87a4b9d734012ca8662708d63
Ivan Lezhankin <ilezhankin@yandex-team.ru>
2015-09-16 20:51:09 +0300

    Implement new versioning scheme for release branches.

--- build/version.py
+++ build/version.py
@@ -1,5 +1,13 @@
 import os
 import subprocess
+import sys
 
 dirname = os.path.dirname(os.path.abspath(__file__))
-print len(subprocess.check_output("git -C {} log --oneline master".format(dirname), shell=True).splitlines())
+merge_base = subprocess.check_output("git -C {} merge-base HEAD master".format(dirname), shell=True).split()[0]
+major = len(subprocess.check_output("git -C {} log --oneline {}".format(dirname, merge_base), shell=True).splitlines())
+
+if subprocess.check_output("git -C {} rev-parse HEAD".format(dirname), shell=True).split()[0] == merge_base:
+    sys.stdout.write(str(major) + ".0")
+
+minor = len(subprocess.check_output("git -C {} log --oneline {}..HEAD".format(dirname, merge_base), shell=True).splitlines())
+sys.stdout.write(str(major) + "." + str(minor))
