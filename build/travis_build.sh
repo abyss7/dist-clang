2373d96cc76e285bde2ba4e29634a3c4a9b555a4
Andrey Malets <andrey.malets@gmail.com>
2016-04-15 05:18:48 +0500

    Update version.py to honour non-existent master branch.

--- build/travis_build.sh
+++ build/travis_build.sh
@@ -2,18 +2,16 @@
 
 set -ex
 
-# Trusty's clang has no 3.8 version, so make symlinks ourselves.
+# Trusty's clang package has no 3.8 version, so make symlinks ourselves.
 for binary in clang clang++ llvm-symbolizer; do
     sudo ln -s "/usr/bin/${binary}-3.8" "/usr/bin/${binary}"
 done
 
-export PATH=/usr/bin:$PATH
-
 build_dir="$(dirname $0)"
 
-# Dirty hack
-git checkout -b master
-
 "$build_dir/configure"
-ninja -C "$build_dir/../out/Test.gn" Tests
+
+# Use system clang for the build.
+PATH=/usr/bin:$PATH ninja -C "$build_dir/../out/Test.gn" Tests
+
 "$build_dir/run_all_tests"
