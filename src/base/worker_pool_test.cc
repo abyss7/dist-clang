725a921e290f4af4a8116a2e17449cbe5da9685a
Andrey Malets <andrey.malets@gmail.com>
2016-06-17 02:48:56 +0500

    ISSUE-58: add a test for worker pool to exit early.

---
+++ src/base/worker_pool_test.cc
@@ -0,0 +1,35 @@
+#include <base/thread.h>
+#include <base/worker_pool.h>
+
+#include <third_party/gtest/exported/include/gtest/gtest.h>
+
+namespace dist_clang {
+namespace base {
+
+TEST(WorkerPoolTest, InstantExitOnShutdown) {
+  Mutex mutex;
+  std::condition_variable condition;
+  bool ready = false;
+  bool done = false;
+  UniquePtr<WorkerPool> pool(new WorkerPool(true));
+
+  UniqueLock lock(mutex);
+
+  pool->AddWorker("TestWorker"_l, [&] (const WorkerPool& pool) {
+    UniqueLock lock(mutex);
+    condition.wait(lock, [&ready] { return ready; });
+    EXPECT_TRUE(pool.WaitUntilShutdown(std::chrono::seconds(5)));
+    done = true;
+    condition.notify_one();
+  });
+
+  Thread thread("Test"_l, [&] { pool.reset(); });
+  ready = true;
+  condition.wait_for(lock, std::chrono::seconds(1), [&done] { return done; });
+  ASSERT_TRUE(done);
+
+  thread.join();
+}
+
+}  // namespace base
+}  // namespace dist_clang
