49bb6b6884c8ea80f5dec62f3b335d898a6d47e8
Ivan Lezhankin <ilezhankin@yandex-team.ru>
2016-10-17 16:53:40 +0300

    ISSUE-84: Use Chromium Clang on Mac too.

--- build/travis.sh
+++ build/travis.sh
@@ -7,16 +7,20 @@ root_dir="$(cd "$(dirname $0)/.." && pwd -P)"
 "$root_dir/build/configure"
 
 if [[ "$(uname)" == Linux ]]; then
-    clang_revision=283753-1
-    clang_root="$root_dir/out/clang-$clang_revision"
-    if ! test -d "$clang_root"; then
-        mkdir "$clang_root"
-        base_url=https://commondatastorage.googleapis.com/chromium-browser-clang
-        curl "$base_url/Linux_x64/clang-${clang_revision}.tgz" |
-            tar -C "$clang_root" -xzf -
-    fi
-    export PATH=$clang_root/bin:$PATH
+    arch="Linux_x64"
+elif [[ "$(uname)" == Darwin ]]; then
+    arch="Mac"
 fi
+
+clang_revision=283753-1
+clang_root="$root_dir/out/clang-$clang_revision"
+if ! test -d "$clang_root"; then
+    mkdir "$clang_root"
+    base_url="https://commondatastorage.googleapis.com/chromium-browser-clang"
+    curl "$base_url/$arch/clang-${clang_revision}.tgz" |
+        tar -C "$clang_root" -xzf -
+fi
+export PATH=$clang_root/bin:$PATH
 
 git -C $root_dir fetch origin master
 ninja -C "$root_dir/out/Test.gn" Tests
