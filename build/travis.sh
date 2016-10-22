895d75c705583a0bbec8708bf3012b4ef9053015
abyss7 <abyss.7@gmail.com>
2016-10-22 17:38:45 +0300

    ISSUE-84: Add build script for Travis. (#85)
    * ISSUE-84: Add build script for Travis.
    
    Taken from https://github.com/andrey-malets/dist-clang/tree/travis-only
    
    * ISSUE-84: chmod +x build/travis.sh
    
    * ISSUE-84: Use Chromium Clang on Mac too.
    
    * Enable -nostdinc++ in Mac builds.
    
    * Fix build on MacOS X 10.12
    
    * ISSUE-84: Use default osx_image and build all configurations.
    
    * ISSUE-84: Build all configurations.
    
    Add some logs for debugging.
    
    * ISSUE-84: don't build packages for now - Travis does some weird stuff.
    
    * More debug logs.
    
    * Install 'tree' on Linux.
    
    * Don't stop execution on first failure.
    
    * Try to detect build race.
    
    * Add value to the '-k' argument
    
    * Fix proto dependencies - at last!
    
    * Update travis.sh
    
    * Make 'base_proto' a shared library.

---
+++ build/travis.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+export PS4="☢"
+
+set -ex
+
+root_dir="$(cd "$(dirname $0)/.." && pwd -P)"
+
+"$root_dir/build/configure"
+
+if [[ "$(uname)" == Linux ]]; then
+    arch="Linux_x64"
+elif [[ "$(uname)" == Darwin ]]; then
+    arch="Mac"
+fi
+
+clang_revision=283753-1
+clang_root="$root_dir/out/clang-$clang_revision"
+if ! test -d "$clang_root"; then
+    mkdir "$clang_root"
+    base_url="https://commondatastorage.googleapis.com/chromium-browser-clang"
+    curl "$base_url/$arch/clang-${clang_revision}.tgz" |
+        tar -C "$clang_root" -xzf -
+fi
+export PATH="$clang_root/bin:$PATH"
+
+git -C "$root_dir" fetch origin master
+ninja -C "$root_dir/out/Debug.gn" All
+ninja -C "$root_dir/out/Release.gn" All # TODO: also build 'Packages'
+ninja -C "$root_dir/out/Test.gn" Tests
+
+"$root_dir/build/run_all_tests" --test-launcher-bot-mode
