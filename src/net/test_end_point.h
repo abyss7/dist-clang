8f807b47173b3f812d51c36605e6553243a4db9e
Ivan Lezhankin <ilezhankin@yandex-team.ru>
2016-12-16 12:37:55 +0300

    Move |conf_| to |BaseDaemon| since everyone uses it - and make it thread-safe.
    Also implement testable |EndPoint| - for future tests with |Connect()|.

---
+++ src/net/test_end_point.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <net/end_point.h>
+
+namespace dist_clang {
+namespace net {
+
+// TODO(ilezhankin): rewrite this stuff using |Testable| and factories, if possible.
+class TestEndPoint : public EndPoint {
+ public:
+  TestEndPoint(const String& host, ui16 port = 0u) : host_(host), port_(port) {}
+  String Print() const override { return host_ + ":" + std::to_string(port_); }
+
+ private:
+  const String host_;
+  const ui16 port_;
+};
+
+}  // namespace net
+}  // namespace dist_clang
