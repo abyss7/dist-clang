168b0be5f0f08800a2be0439dcecc790244269bb
Ivan Lezhankin <ilezhankin@yandex-team.ru>
2019-02-04 22:46:41 +0300

    Install more recent ninja tools

--- build/deps.sh
+++ build/deps.sh
@@ -7,9 +7,13 @@ set -ex
 if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
     curl -JLO https://chrome-infra-packages.appspot.com/dl/gn/gn/linux-amd64/+/T09VSJ7uJnfkIHG3v7o0oYjo0nWLwvFnR05-I9iRU-EC
     unzip -q gn-linux-amd64.zip
+    curl -JLO https://github.com/ninja-build/ninja/releases/download/v1.8.2/ninja-linux.zip
+    unzip -q ninja-linux.zip
 fi
 
 if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
     curl -JLO https://chrome-infra-packages.appspot.com/dl/gn/gn/mac-amd64/+/EDk770Anp4Qo6I8K2CaZA-aJcEjZ6pAqD7QSqcoEuIMC
     unzip -q gn-mac-amd64.zip
+    curl -JLO https://github.com/ninja-build/ninja/releases/download/v1.8.2/ninja-mac.zip
+    unzip -q ninja-mac.zip
 fi
--- build/version.py
+++ build/version.py
@@ -3,7 +3,7 @@ import subprocess
 import sys
 
 dirname = os.path.dirname(os.path.abspath(__file__))
-merge_base = subprocess.check_output("git -C {} merge-base HEAD origin/master".format(dirname), shell=True).split()[0]
+merge_base = subprocess.check_output("git -C {} merge-base HEAD master".format(dirname), shell=True).split()[0]
 major = len(subprocess.check_output("git -C {} log --oneline {}".format(dirname, merge_base), shell=True).splitlines())
 
 if subprocess.check_output("git -C {} rev-parse HEAD".format(dirname), shell=True).split()[0] == merge_base:
