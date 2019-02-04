b292bcd25f8f7f79c5d14208a8afce62de5999cc
Ivan Lezhankin <ilezhankin@yandex-team.ru>
2019-02-04 21:27:49 +0300

    Update Travis scripts

--- build/deps.sh
+++ build/deps.sh
@@ -5,11 +5,11 @@ export PS4="☢"
 set -ex
 
 if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
-    curl -JLO https://chrome-infra-packages.appspot.com/dl/gn/gn/linux-amd64/+/T09VSJ7uJnfkIHG3v7o0oYjo0nWLwvFnR05-I9iRU-EC;
-    unzip -q gn-linux-amd64.zip;
+    curl -JLO https://chrome-infra-packages.appspot.com/dl/gn/gn/linux-amd64/+/T09VSJ7uJnfkIHG3v7o0oYjo0nWLwvFnR05-I9iRU-EC
+    unzip -q gn-linux-amd64.zip
 fi
 
 if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
-    curl -JLO https://chrome-infra-packages.appspot.com/dl/gn/gn/mac-amd64/+/EDk770Anp4Qo6I8K2CaZA-aJcEjZ6pAqD7QSqcoEuIMC;
-    unzip -q gn-mac-amd64.zip;
+    curl -JLO https://chrome-infra-packages.appspot.com/dl/gn/gn/mac-amd64/+/EDk770Anp4Qo6I8K2CaZA-aJcEjZ6pAqD7QSqcoEuIMC
+    unzip -q gn-mac-amd64.zip
 fi
