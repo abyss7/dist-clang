a3377793b7e13e5b9a61a2d31773d5fa132b8fc6
Ivan Lezhankin <ilezhankin@yandex-team.ru>
2019-02-04 23:02:29 +0300

    Remove possibly unused chunk of script

--- build/travis.sh
+++ build/travis.sh
@@ -9,12 +9,6 @@ export PATH="$PATH:$TRAVIS_BUILD_DIR"
 
 "$root_dir/build/configure"
 
-if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
-    arch="Linux_x64"
-elif [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
-    arch="Mac"
-fi
-
 git -C "$root_dir" fetch origin master
 ninja -C "$root_dir/out/Debug.gn" All
 ninja -C "$root_dir/out/Release.gn" All # TODO: also build 'Packages'
