e4fdf32ca39e17c178eca2eb47af61c9851063f5
Ivan <5627721+abyss7@users.noreply.github.com>
2019-03-03 18:55:29 +0300

    Fix Travis build (#125)
    * Update .travis.yml
    * Install binary 'gn' before running build
    * Update Travis scripts
    * Install more recent ninja tools
    * Don't expand commands in shell-scripts
    * Remove possibly unused chunk of script

--- build/travis.sh
+++ build/travis.sh
@@ -1,28 +1,13 @@
 #!/usr/bin/env bash
 
-export PS4="☢"
-
-set -ex
+set -e
 
 root_dir="$(cd "$(dirname $0)/.." && pwd -P)"
 
+# gn and ninja are extracted into build dir
+export PATH="$PATH:$TRAVIS_BUILD_DIR"
+
 "$root_dir/build/configure"
-
-if [[ "$(uname)" == Linux ]]; then
-    arch="Linux_x64"
-elif [[ "$(uname)" == Darwin ]]; then
-    arch="Mac"
-fi
-
-clang_revision=299960-1
-clang_root="$root_dir/out/clang-$clang_revision"
-if ! test -d "$clang_root"; then
-    mkdir "$clang_root"
-    base_url="https://commondatastorage.googleapis.com/chromium-browser-clang"
-    curl "$base_url/$arch/clang-${clang_revision}.tgz" |
-        tar -C "$clang_root" -xzf -
-fi
-export PATH="$clang_root/bin:$PATH"
 
 git -C "$root_dir" fetch origin master
 ninja -C "$root_dir/out/Debug.gn" All
